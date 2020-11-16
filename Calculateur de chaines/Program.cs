da85e4b267f93f79857a3efeeb431dc50b9a9e94
Gauthier Marescaux <marescaux.gauthier@gmail.com>
2020-11-16 08:48:00 +0100

    (init) creation du projet

---
+++ Calculateur de chaines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculateur_de_chaines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
